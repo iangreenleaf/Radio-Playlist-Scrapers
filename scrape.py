acb722ca9946e7abddca37c620a44cac440e6041
youngian <youngian@guevara.(none)>
2008-11-21 23:14:51 -0600

    Parse am/pm for times

--- scrape.py
+++ scrape.py
@@ -57,12 +57,24 @@ def songs_for_day(date):
 	page = urllib.urlopen("http://minnesota.publicradio.org/radio/services/the_current/songs_played/?month="+str(date.month)+"&day="+str(date.day)+"&year="+str(date.year))
 
 	soup = BeautifulSoup(page.read(), convertEntities=BeautifulSoup.HTML_ENTITIES)
-	divs = soup.findAll('div', { "class" : ["playlist_time", "playlist_title"] })
+	divs = soup.findAll(['div', 'h3'], { "class" : ["playlist_time", "playlist_title", "header"] })
 	for div in divs:
-		if div['class'] == "playlist_time":
+		if div['class'] == "header" and div.name == "h3":
+			pm_re = re.compile('(11.*12.*AM)|PM')
+			fake_pm_re = re.compile('(11.*12.*PM)')
+			if div.string and pm_re.search(div.string) and not fake_pm_re.search(div.string):
+				post_meridiem = True
+			else:
+				post_meridiem = False
+		elif div['class'] == "playlist_time":
 			#play_time = datetime.time(div.string)
-			play_time = div.string
-		else:
+			timebits = map(int, div.string.split(':'))
+			if post_meridiem and timebits[0] < 12:
+				timebits[0] += 12
+			if not post_meridiem and timebits[0] == 12:
+				timebits[0] = 0
+			play_time = datetime.time(timebits[0], timebits[1])
+		elif div['class'] == "playlist_title":
 			artist = div.a.string
 			song_title = ''.join(div.findAll(text=True, recursive=False))
 			song_title = re.sub(r'\s*-\s+', r'', song_title)
@@ -71,7 +83,7 @@ def songs_for_day(date):
 				continue
 			artist = normalize(artist)
 			song_title = normalize(song_title)
-			yield (play_time, artist, song_title)
+			yield (play_time.isoformat(), artist, song_title)
 
 def dump_db():
 	connection = sqlite3.connect('tmpdb')
