b149d1d89756089c512dcc710e9888b6183ec52b
youngian <youngian@allende.localdomain>
2009-02-14 20:58:55 -0600

    Version on server, using fixed timezones and some other things

--- scrape_clearchannel.py
+++ scrape_clearchannel.py
@@ -1,19 +1,24 @@
-#!/usr/bin/python
+#!/home/youngian/local/bin/python
 import urllib
 from BeautifulSoup import BeautifulSoup
-import sqlite3
+from pysqlite2 import dbapi2
 import re
 import datetime
+from pytz import timezone
+import pytz
 import sys
 
 
 def append_songs(db, url, cushion_minutes=60):
-	connection = sqlite3.connect(db)
+	connection = dbapi2.connect(db)
 	c = connection.cursor()
 
 	thisdate = datetime.date.today()
 	cushiondelta = datetime.timedelta(minutes=cushion_minutes)
-	cushion = datetime.datetime.now() - cushiondelta
+	cushion = datetime.datetime.now(tz=pytz.utc)
+	central = timezone('US/Central')
+	cushion = cushion.astimezone(central)
+	cushion = cushion - cushiondelta
 
 	c.execute('''select title from songs\
 			where date_played >= ?\
@@ -31,9 +36,13 @@ def append_songs(db, url, cushion_minutes=60):
 		if song[2] in recentsongs:
 			continue
 		else:
+			date = song[0].date()
+			time = song[0].time()
+			artist = song[1]
+			title = song[2]
 			c.execute('insert into songs\
 					(date_played, time_played, artist, title)\
-					values (?, ?, ?, ?)', (thisdate.isoformat(),) + song)
+					values (?, ?, ?, ?)', (date.isoformat(), time.isoformat(), artist, title))
 			count = count + 1
 
 	c.execute('insert into parsed_info\
@@ -63,9 +72,11 @@ def last_10_songs(url):
 			continue
 		artist = normalize(artist)
 		song_title = normalize(song_title)
-		time_parsed = datetime.datetime.now().time()
-		time_parsed = time_parsed.replace(microsecond = 0, second = 0)
-		yield (time_parsed.isoformat(), artist, song_title)
+		dt_parsed = datetime.datetime.now(tz=pytz.utc)
+		central = timezone('US/Central')
+		dt_parsed = dt_parsed.astimezone(central)
+		dt_parsed = dt_parsed.replace(microsecond = 0, second = 0)
+		yield (dt_parsed, artist, song_title)
 		#yield (datetime.datetime.now().time().strftime('%H:%M:%S'), artist, song_title)
 
 
@@ -83,21 +94,10 @@ def normalize(string):
 	string = string.strip()
 	return string
 
-# Just spit out some summary info to make sure things went okay
-def dump_db():
-	connection = sqlite3.connect('tmpdb2')
-	c = connection.cursor()
-	c.execute('select * from parsed_info')
-	for line in c:
-		print line
-	c.close()
-
-
 stations = {'cities97': 'http://www.cities97.com/iplaylist/playlist.html?last10=1',
 		'kdwb': 'http://www.kdwb.com/iplaylist/playlist.html?last10=1',
 		'kool108': 'http://www.kool108.com/iplaylist/playlist.html?last10=1'}
 for key in stations:
-	db = './' + key + '.sqlite'
+	db = '/home/youngian/scraper/' + key + '.sqlite'
 	url = stations[key]
 	append_songs(db, url)
-dump_db()
