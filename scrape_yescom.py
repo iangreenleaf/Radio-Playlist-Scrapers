e0540ce22b7c991fb96863bdcccff9b7548cfe38
youngian <youngian@guevara.localdomain>
2009-03-21 20:59:08 -0500

    First working version

--- scrape_yescom.py
+++ scrape_yescom.py
@@ -1,52 +1,31 @@
 #!/home/youngian/local/bin/python
 import urllib
-from BeautifulSoup import BeautifulSoup
 from pysqlite2 import dbapi2
 import re
 import datetime
-from pytz import timezone
-import pytz
 import sys
+import simplejson
 
 debug = False
 
-def append_songs(db, url, cushion_minutes=60):
+def append_songs(db, url):
 	connection = dbapi2.connect(db)
 	c = connection.cursor()
 
 	thisdate = datetime.date.today()
 
-	c.execute('''select title,artist from songs\
-			order by date_played desc,time_played desc
-			limit 10''')
-
-	recentsongsresult = c.fetchall()
-	recentsongs = []
-	for row in recentsongsresult:
-		recentsongs.append(row[1] + row[0])
-
-	# Let's set up to do a bit of sanity checking
-	dupe_found = False
-	dupe_warning = False
-	dupes = []
-	inserted = []
+	#c.execute('select next_id from last_parsed order by date desc limit 1');
+	#nextAt = c.fetchone()[0]
+	nextAt = 31553
+	url = url + '/ev/' + str(nextAt)
 
 	count = 0
 	for song in last_10_songs(url):
-		if song[1] + song[2] in recentsongs:
-			dupe_found = True
-			dupes.append(song[1] + song[2])
-			continue
-		else:
-			if dupe_found:
-				dupe_warning = True
-			# This should keep our songs in the order they were parsed
-			dt = song[0] - datetime.timedelta(minutes = count)
+			dt = song[0]
 			date = dt.date()
 			time = dt.time()
 			artist = song[1]
 			title = song[2]
-			inserted.append(song)
 			c.execute('insert into songs\
 					(date_played, time_played, artist, title)\
 					values (?, ?, ?, ?)', (date.isoformat(), time.isoformat(), artist, title))
@@ -55,47 +34,31 @@ def append_songs(db, url, cushion_minutes=60):
 	c.execute('insert into parsed_info\
 			values (?, ?)', (thisdate.isoformat(), count))
 
-	if debug:
-			if (len(inserted) > 5):
-				print db + ': Got ' + str(len(inserted)) + ' insertions, which seems odd'
-			if dupe_warning:
-				print db + ': Duplicates detected out of order'
-			if len(inserted) > 5 or dupe_warning:
-				print 'Inserted: ' + str(inserted)
-				print 'Dupes: ' + str(dupes)
-				print 'Recent songs: ' + str(recentsongsresult)
-
 	connection.commit()
 	c.close()
 
-	return len(inserted)
+	return
 
 
 def last_10_songs(url):
 	page = urllib.urlopen(url)
 
-	soup = BeautifulSoup(page.read(), convertEntities=BeautifulSoup.HTML_ENTITIES)
-	rows = soup.findAll(['td'], { "class" : ["playlist_artist"] })
-	for row in rows:
-		song_title = None
-		artist = None
-		bits = row.findAll('a')
-		for bit in bits:
-
-			if bit.b:
-				artist = bit.b.string
-			else:
-				song_title = bit.string
-
+	result = simplejson.load(page)
+	next_id = result['next']
+	songs = result['entries']
+	for row in songs:
+		song_title = row['title']
+		artist = row['by']
 		if artist == None or song_title == None:
 			continue
 		artist = normalize(artist)
 		song_title = normalize(song_title)
-		dt_parsed = datetime.datetime.now(tz=pytz.utc)
-		central = timezone('US/Central')
-		dt_parsed = dt_parsed.astimezone(central)
+		#central = timezone('US/Central')
+		#dt_parsed = datetime.fromtimestamp(row['at'] / 1000, central)
+		dt_parsed = datetime.datetime.fromtimestamp(row['at'] / 1000)
+		#dt_parsed = dt_parsed.astimezone(central)
 		dt_parsed = dt_parsed.replace(microsecond = 0, second = 0)
-		yield (dt_parsed, artist, song_title)
+		yield (dt_parsed, artist, song_title, next_id)
 		#yield (datetime.datetime.now().time().strftime('%H:%M:%S'), artist, song_title)
 
 
@@ -113,14 +76,10 @@ def normalize(string):
 	string = string.strip()
 	return string
 
-stations = {'cities97': 'http://www.cities97.com/iplaylist/playlist.html?last10=1',
-		'kdwb': 'http://www.kdwb.com/iplaylist/playlist.html?last10=1',
-		'kool108': 'http://www.kool108.com/iplaylist/playlist.html?last10=1'}
+stations = {'something': 'http://r2b.yes.com/relay/fead789258e8b63acb4c17cfb5ded00d1fa2a3e9'}
 sum = 0
 for key in stations:
 	db = '/home/youngian/scraper/' + key + '.sqlite'
+	db = 'newtmpdb'
 	url = stations[key]
-	sum += append_songs(db, url)
-
-if debug and sum == 0:
-	print 'No insertions on any station. I\'m vaguely concerned.'
+	append_songs(db, url)
