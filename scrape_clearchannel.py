b66ea865caf2a4fd75fa6013633672c0d1abcda7
youngian <youngian@guevara.localdomain>
2009-06-03 23:43:57 -0500

    Adding GPL text

--- scrape_clearchannel.py
+++ scrape_clearchannel.py
@@ -1,5 +1,21 @@
 #!/home/youngian/local/bin/python
 
+# Ian Young
+# December 2008
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ##
 ## Scrape a variety of Clearchannel-powered radio websites for their
 ## most recently played songs. Should be run once every ~10-20 minutes.
